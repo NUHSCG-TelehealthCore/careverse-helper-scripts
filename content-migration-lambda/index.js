68524b6b8069c723f76d94c45c1e5d38fde112f1
sajeban <sajerex@gmail.com>
2023-11-17 17:01:03 +0800

    lambda updates

--- content-migration-lambda/index.js
+++ content-migration-lambda/index.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
 const mysql = require('mysql2');
-const { S3Client, CopyObjectCommand, ListObjectsCommand } = require("@aws-sdk/client-s3");
 
+exports.handler = (event, context, callback) => {
+    // Prevent the Lambda function from timing out
+    context.callbackWaitsForEmptyEventLoop = false;
 
-
-const migrateArticles = (sourceDBName, targetDBName) => {
+    const sourceDBName = process.env.SOURCE_DB_NAME;
+    const targetDBName = process.env.TARGET_DB_NAME;
 
     if (!sourceDBName || !targetDBName) {
-        return new Promise((resolve, reject) => {
-            return reject("DB names are empty!")
-        })
+        return callback(new Error("DB names are empty!"));
     }
 
     const pool = mysql.createPool({
@@ -19,68 +18,55 @@ const migrateArticles = (sourceDBName, targetDBName) => {
         password: process.env.DB_PASSWORD
     });
 
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            return callback(new Error("Error occurred while getting the connection"));
+        }
+
+        // Begin transaction
+        connection.beginTransaction(err => {
             if (err) {
-                return reject("Error occurred while getting the connection");
+                connection.release();
+                return callback(new Error("Error occurred while creating the transaction"));
             }
-            // BEGIN TRANSACTION
-            return connection.beginTransaction(err => {
+
+            // Execute SQL to delete articles table
+            connection.execute(`DELETE FROM ${targetDBName}.articles`, (err) => {
                 if (err) {
-                    connection.release();
-                    return reject("Error occurred while creating the transaction");
+                    return connection.rollback(() => {
+                        connection.release();
+                        return callback(new Error("Failed to wipe out `articles` table"));
+                    });
                 }
 
-                // EXECUTE SQL TO DELETE `articles` table
-                return connection.execute(`DELETE FROM ${targetDBName}.articles`, (err) => {
-                    if (err) {
-                        return connection.rollback(() => {
-                            connection.release();
-                            return reject("Failed to wipe out `articles` table");
-                        });
-                    }
-                    // PROCEED TO INSERT QUERY EXECUTION
-                    return connection.execute(
-                        `INSERT INTO ${targetDBName}.articles 
-                         (nav_title, sub_nav_title, content, created_at, published_at, title, sub_title) 
-                         SELECT nav_title, sub_nav_title, content, created_at, published_at, title, sub_title from ${sourceDBName}.articles`,
-                        (err) => {
+                // Proceed to insert query execution
+                connection.execute(
+                    `INSERT INTO ${targetDBName}.articles 
+                     (nav_title, sub_nav_title, content, created_at, published_at, title, sub_title) 
+                     SELECT nav_title, sub_nav_title, content, created_at, published_at, title, sub_title 
+                     FROM ${sourceDBName}.articles`,
+                    (err) => {
+                        if (err) {
+                            return connection.rollback(() => {
+                                connection.release();
+                                return callback(new Error("Failed to insert into `articles` table"));
+                            });
+                        }
+
+                        // Commit transaction since no issue
+                        connection.commit(err => {
                             if (err) {
                                 return connection.rollback(() => {
                                     connection.release();
-                                    return reject("Failed to insert to `articles` table");
-                                })
+                                    return callback(new Error("Commit failed"));
+                                });
                             }
-                            // COMMIT TRANSACTION SINCE NO ISSUE
-                            return connection.commit((err) => {
-                                if (err) {
-                                    return connection.rollback(() => {
-                                        connection.release();
-                                        return reject("Commit failed");
-                                    });
-                                }
-                                connection.release();
-                                return resolve("Articles migrated successfully");
-                            })
-                        }
-                    )
-                })
-            })
-        })
-    })
-}
-
-
-const triggerMigration = () => {
-    console.log('Running migration');
-    // start transaction to wipe out article table from dev
-    migrateArticles(process.env.SOURCE_DB_NAME, process.env.TARGET_DB_NAME)
-        .then(result => {
-            console.log(result);
-        })
-        .catch(err => {
-            console.log(err);
+                            connection.release();
+                            return callback(null, "Articles migrated successfully");
+                        });
+                    }
+                );
+            });
         });
-}
-
-triggerMigration();
+    });
+};
